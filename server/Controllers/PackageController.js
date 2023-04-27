1a0f0d4158bce5a39b27520151e9c1d2abcad34b
daniel200292 <daniel2002921@gmail.com>
2023-04-27 15:58:24 +0800

    2023/4/27 PackageController - 優化index API與新增住戶資料回傳

--- server/Controllers/PackageController.js
+++ server/Controllers/PackageController.js
@@ -5,68 +5,62 @@ const LoginController = {
     // 傳入參數 req, res
 
     index: (req, res) => {
-      let result = {}
+      
       // res.send("dsfds")
       // console.log(req.body)
       // res.send(req.body.username)
+      
       res.setHeader('Content-Type', 'application/json');
+      (async () => {
+        let result = {}
+        try{
+            
+            const pool = await sql.connect(Sql_config)
+            const request = pool.request();
 
-      try{
-
-        sql.connect(Sql_config,function (err) {
-            if(err) console.log(err);
-              //create Request object
-              
-              var request=new sql.Request();
-              let sqlquery = 'select top(20) * from package'
-              request.query(sqlquery,function(err,recordset){
-              if(err) console.log(err);
-
-            //   console.log(recordset);
-
-
-              
-              
-              if(recordset.rowsAffected[0]!=0){
-                console.log(recordset.recordset)
-                // let sqlUsername = recordset.recordset[0].account
-                // let sqlPassword = recordset.recordset[0].password
-
-                // if (keyInPassword!=sqlPassword){
-                //   result.status = "error"
-                //   result.message = "密碼輸入錯誤"
-                // }
-                // if(keyInPassword == sqlPassword){
-                //   result.status = "success"
-                //   result.message = "登入成功"
-                // }
-                result.status = "success"
-                result.data = recordset.recordset
-
-
-              }else{
-                  console.log("8888")
-                  result.status = "error"
-                  result.message = "no data"
-              }
-
-              result = JSON.stringify(result)
-              res.send(result)
-              // if(recordset.recordset.length!=0){
-                
-              // }
-              //send records as a response
+            //getTableData
+            let sqlquery = 'select top(20) * from package'
+            let recordset = await request.query(sqlquery)
+            result.tableData = {}
+            if(recordset.rowsAffected[0]!=0){
               // console.log(recordset.recordset)
-              // res.send(recordset.recordset);
-              });
-          });
+              result.tableData.status = "success"
+              result.tableData.data = recordset.recordset
 
-      }catch(err){
-        result.status = "error"
-        console.log("32444444")
-        result = JSON.stringify(result)
-        res.send(result)
-      }
+            }else{
+                result.tableData.status = "error"
+                result.tableData.message = "no Tabledata"
+            }
+
+            //getNeighborData
+            sqlquery = 'select * from neighbor'
+            recordset = await request.query(sqlquery)
+            result.neighborData = {}
+            if(recordset.rowsAffected[0]!=0){
+              // console.log(recordset.recordset)
+              result.neighborData.status = "success"
+              result.neighborData.data = recordset.recordset
+
+            }else{
+                result.neighborData.status = "error"
+                result.neighborData.message = "no NeighborData"
+            }
+
+      
+  
+            result.status = "success"
+            console.log(result)
+            result = JSON.stringify(result)
+            res.send(result)
+  
+        }catch(err){
+          result.status = "error"
+          result.message = err
+          result = JSON.stringify(result)
+          res.send(result)
+        }
+      })()
+      
       
 
 
